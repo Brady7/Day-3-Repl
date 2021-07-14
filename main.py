edac1eb5bdae015bb805446c7d64dc877cf1ad1d
BMReplit <87283212+Brady7@users.noreply.github.com>
2021-07-14 22:41:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+numberCycle=1
+# numberTriangle=numberCycle+1
+# while numberTriangle<5:
+#   numberTriangle=numberCycle+1
+#   break
+# print (numberCycle)and print (numberTriangle)
+  
+# number=input ("Type in a number!")
+# number=int(number)
+# if number<0:
+#   print ("This number is negative!")
+# elif number==0:
+#   print ("0 is neither positive or negative!")
+# elif number>0:
+#   print ("This number is positive!")
+# ----------------------------------------------------
+
+# def multiplyNumbers (inputNumber1, inputNumber2):
+#     answerValue=int(inputNumber1)*int(inputNumber2)
+#     return answerValue
+
+# inputNumber1=input ("What is the first number?")
+# inputNumber2=input ("What is the second number?")
+# print (multiplyNumbers(inputNumber1, inputNumber2))
+
+
+import turtle
+turtle.speed(0.5)
+turtle=turtle.Turtle()
+turtle.forward(70)
+turtle.left (130)
+turtle.forward(70)
+turtle.left (105)
+turtle.forward(70)
